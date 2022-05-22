06006ebecd2fa8e7ee59b792fa9c37b7f07ef36c
robertoCastro <bkastro@gmail.com>
2022-05-22 16:31:50 -0300

    Programa java verificando se um número é divisível pelo outro

---
+++ ehDivisivel/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Integer num1 = 20;
+        Integer num2 = 10;
+        if(num1 % num2 == 0){
+            System.out.println("O primeiro número é divisível pelo segundo");
+        } else {
+            System.out.println("O primeiro numero não é divisível pelo segundo");
+        }
+
+    }
+
+}
